7e5cd9ae5ed8a35ec801ddf57a7e391b567316b5
777uliahshafar <777uliahshafar@gmail.com>
2023-07-15 18:59:26 +0800

    concealbrackettex

--- lua/gruvbox/groups.lua
+++ lua/gruvbox/groups.lua
@@ -92,7 +92,7 @@ M.setup = function()
     TabLine = { link = "TabLineFill" },
     MatchParen = { bg = colors.bg3, bold = config.bold },
     ColorColumn = { bg = colors.bg1 },
-    Conceal = { fg = colors.blue },
+    Conceal = { fg = colors.fg2 },
     CursorLineNr = { fg = colors.fg5, bg = nil },
     NonText = { link = "GruvboxBg2" },
     SpecialKey = { link = "GruvboxFg4" },
