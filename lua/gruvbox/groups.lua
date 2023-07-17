756c121fda802e179771ded27ba189b3824b5447
777uliahshafar <777uliahshafar@gmail.com>
2023-07-17 18:54:07 +0800

    choco

--- lua/gruvbox/groups.lua
+++ lua/gruvbox/groups.lua
@@ -117,7 +117,7 @@ M.setup = function()
     Question = { link = "GruvboxOrangeBold" },
     WarningMsg = { link = "GruvboxRedBold" },
     LineNr = { fg = colors.fg4 },
-    SignColumn = config.transparent_mode and {fg = colors.fg4, bg = nil } or {fg = colors.fg4, bg = colors.choco, reverse = config.inverse },
+    SignColumn = config.transparent_mode and {fg = colors.fg4, bg = nil } or {fg = colors.fg4, bg = colors.choco },
     Folded = { fg = colors.fg8, bg = colors.bg8, italic = config.italic.folds },
     FoldColumn = config.transparent_mode and { fg = colors.gray, bg = nil } or { fg = colors.gray, bg = colors.bg1 },
     Cursor = { reverse = config.inverse },
