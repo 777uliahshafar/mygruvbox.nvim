660b682c108633a9eb7eb9f660a53d708cb193d8
777uliahshafar <777uliahshafar@gmail.com>
2024-04-02 21:55:12 +0800

    rmobsidian

--- lua/gruvbox/groups.lua
+++ lua/gruvbox/groups.lua
@@ -1051,11 +1051,11 @@ M.setup = function()
     texCmdType = { link = "texCmd" },
 
     --obsidian
-    ObsidianTodo = { link = "GruvboxGray" },
-    ObsidianDone = { link = "texEnvArgName" },
-    ObsidianRightArrow = { link = "texCmd" },
-    ObsidianRefText = { link = "texOptSep" },
-    ObsidianBullet = { link = "GruvboxOrange" },
+    -- ObsidianTodo = { link = "GruvboxGray" },
+    -- ObsidianDone = { link = "texEnvArgName" },
+    -- ObsidianRightArrow = { link = "texCmd" },
+    -- ObsidianRefText = { link = "texOptSep" },
+    -- ObsidianBullet = { link = "GruvboxOrange" },
   }
 
   for group, hl in pairs(config.overrides) do
