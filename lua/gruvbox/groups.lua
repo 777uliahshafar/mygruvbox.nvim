92d63f2f6070b30c7355091e5805266236cbbc9d
777uliahshafar <777uliahshafar@gmail.com>
2024-04-03 05:32:55 +0800

    noobsidianspace2

--- lua/gruvbox/groups.lua
+++ lua/gruvbox/groups.lua
@@ -173,12 +173,6 @@ M.setup = function()
     SpellLocal = { fg = colors.aqua, bg = nil, italic = config.italic.comments, reverse = config.invert_signs },
     SpellRare = { fg = colors.purple, bg = nil, reverse = config.invert_signs },
     Whitespace = { fg = colors.fg4 },
-    --obsidian
-    ObsidianTodo = { link = "GruvboxGray" },
-    ObsidianDone = { link = "GruvboxAqua" },
-    ObsidianRightArrow = { link = "GruvboxGreen" },
-    ObsidianRefText = { link = "GruvboxBlue" },
-    ObsidianBullet = { link = "GruvboxOrange" },
     -- LSP Diagnostic
     DiagnosticError = { link = "GruvboxRed" },
     DiagnosticSignError = { link = "GruvboxRedSign" },
