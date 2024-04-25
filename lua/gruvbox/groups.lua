872de801b16694bf73df0c76476ecda1a2d84ed9
777uliahshafar <777uliahshafar@gmail.com>
2024-04-25 21:15:42 +0800

    chunk

--- lua/gruvbox/groups.lua
+++ lua/gruvbox/groups.lua
@@ -479,7 +479,7 @@ M.setup = function()
     NvimTreeOpenedFile = { fg = colors.bright_red, bold = true },
     NvimTreeSpecialFile = { fg = colors.neutral_yellow, bold = true, underline = true },
     NvimTreeImageFile = { fg = colors.neutral_purple },
-    NvimTreeIndentMarker = { fg = colors.blue },
+    NvimTreeIndentMarker = { fg = colors.chunk },
     NvimTreeGitDirty = { fg = colors.neutral_yellow },
     NvimTreeGitStaged = { fg = colors.neutral_yellow },
     NvimTreeGitMerge = { fg = colors.neutral_purple },
