9e257af688417c4c6ebc0961bb89de64eb50bfd5
777uliahshafar <777uliahshafar@gmail.com>
2024-09-04 07:22:25 +0800

    same

--- lua/gruvbox/groups.lua
+++ lua/gruvbox/groups.lua
@@ -1056,7 +1056,7 @@ M.setup = function()
     texCmdTitle = { link = "texCmd" },
     texCmdItem = { link = "texCmd" },
     texCmdRef = { link = "texCmdEnv" },
-    texPartArgTitle = { link = "GruvboxRed" },
+    texPartArgTitle = { link = "texCmdEnv" },
     texRefArg = { link = "texCmd" },
     texCmdPart = { link = "texCmdEnv" },
     texCmdMath = { link = "texCmdEnv" },
