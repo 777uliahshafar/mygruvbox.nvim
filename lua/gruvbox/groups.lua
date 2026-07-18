1e971741de21a0597a7d8d159a3e0d0462c5abde
777uliahshafar <777uliahshafar@gmail.com>
2026-07-18 18:03:33 +0800

    gclist2

--- lua/gruvbox/groups.lua
+++ lua/gruvbox/groups.lua
@@ -133,7 +133,7 @@ M.setup = function()
     vCursor = { link = "Cursor" },
     iCursor = { link = "Cursor" },
     lCursor = { link = "Cursor" },
-    Special = { link = "GruvboxBlue" },
+    Special = { link = "GruvboxOrange" },
     Comment = { fg = colors.fg7, italic = config.italic.comments },
     -- Todo         anything that needs extra attention; mostly the
     --              keywords TODO FIXME and XXX
